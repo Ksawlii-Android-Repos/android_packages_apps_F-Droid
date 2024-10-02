819297aeff66b7f5c4380b2e222a909ec3849bcf
Ksawlii <ksawlii.do.plikow@gmail.com>
2024-10-02 12:06:18 +0200

    Initial Release

---
+++ Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := ViaBrowser
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := fdroid.apk
+LOCAL_MODULE_CLASS := APPS
+#LOCAL_PRIVILEGED : true
+include $(BUILD_PREBUILT)
